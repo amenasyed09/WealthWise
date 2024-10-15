7edd36a18ea33483e37634e98db59db93ecfd73d
amenasyed09 <160035408+amenasyed09@users.noreply.github.com>
2024-10-15 15:24:22 +0530

    bcrypt

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const UserModel = require('./Models/User');
 const incomeModel=require('./Models/Income');
 const Expense = require('./Models/Expense'); 
