b709f410061940f48b562a631942dbebab90f033
amenasyed09 <160035408+amenasyed09@users.noreply.github.com>
2024-10-16 16:06:11 +0530

    Create index.js

---
+++ Backend/api/index.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = require('../app'); // Import the app.js from the backend folder
+
+module.exports = app; // Export the app as the serverless function
