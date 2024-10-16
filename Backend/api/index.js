2f0148fec5d17ca946c4dbb37464e7990dc6475b
amenasyed09 <160035408+amenasyed09@users.noreply.github.com>
2024-10-16 16:15:36 +0530

    No vercel

--- Backend/api/index.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const app = require('../app'); // Import the app.js from the backend folder
-
-module.exports = app; // Export the app as the serverless function
