09ad497ce193bf4c0558d4fd9e2d19e09f6a66ca
amenasyed09 <160035408+amenasyed09@users.noreply.github.com>
2024-10-22 20:42:04 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -663,6 +663,6 @@ app.post('/api/auth/google-login', async (req, res) => {
 });
 
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
