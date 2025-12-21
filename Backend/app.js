c832b81894b99c044986a440493fd37b1909d3b9
amenasyed09 <160035408+amenasyed09@users.noreply.github.com>
2025-12-21 18:19:37 +0530

    update cors

--- Backend/app.js
+++ Backend/app.js
@@ -14,7 +14,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin:  process.env.FRONTEND_URL || '*',
   credentials: true
 }));
 app.use(express.json());
